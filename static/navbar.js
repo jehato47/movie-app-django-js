77d013344812a98f76ce69534f5fd361381562c8
jehat armanç deniz <45170719+jehato47@users.noreply.github.com>
2020-05-18 18:27:30 +0300

    Update navbar.js

--- static/navbar.js
+++ static/navbar.js
@@ -4,7 +4,6 @@ let search3 = "&include_adult=false"
 
 
 
-
 document.getElementById("search_form").addEventListener("submit", (e) => {
     target = document.getElementById("search").value
     url = search + target + search2 + "1" + search3
@@ -14,38 +13,13 @@ document.getElementById("search_form").addEventListener("submit", (e) => {
     let cards = document.getElementById("cards")
 
     // This is because if you go to details it will clear secondary div
-    cards != undefined ?  cards.innerHTML = "": document.getElementById("secondary-div").innerHTML=""
-    cards != undefined ?  cards.innerHTML = "": document.getElementById("secondary-div").innerHTML+=cards
+    cards !== undefined ?  cards.innerHTML = "": document.getElementById("secondary-div").innerHTML=""
+    cards !== undefined ?  cards.innerHTML = "": document.getElementById("secondary-div").innerHTML+=cards
 
     fulfill_cards(search + target + search2, "secondary-div")
 
     e.preventDefault()
 })
-
-// let s = 1;
-// let target = ""
-// document.getElementById("search").addEventListener("keydown", (e) => {
-//     if(e.key === "Enter"){
-//         if(s===1 && document.getElementById("loadmore")){document.getElementById("loadmore").remove()
-//         s+=1
-//         console.log(s)
-//     }
-//         target = e.target.value
-//         url = search + e.target.value + search2 + "1" + search3
-//         fetch(url)
-//         .then(response => response.text())
-//         .then(response => console.log(JSON.parse(response)))
-//         document.getElementById("cards").innerHTML = ""
-//         fulfill_cards(search + e.target.value + search2)
-//     }
-//     console.log(e.key)
-// })
-
-
-
-
-
-
 
 
 
